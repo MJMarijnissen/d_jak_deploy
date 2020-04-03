e029614592ab53caa717de57b2baa581d55e3dde
MJMarijnissen <kubamarijnissen@gmail.com>
2020-04-03 14:28:12 +0200

    zadanie 2 ZD wykonane

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ def hello_world():
 @app.get('/hello/{name}')
 def hello_name(name: str):
 	return {"message": f"Hello {name}"}
+
+@app.get('/method')
+def method_return():
+	return {"method": "GET"}
+
+@app.post('/method')
+def method_return():
+	return {"method": "POST"}
+
+@app.put('/method')
+def method_return():
+	return {"method": "PUT"}
+
+@app.delete('/method')
+def method_return():
+	return {"method": "DELETE"}
