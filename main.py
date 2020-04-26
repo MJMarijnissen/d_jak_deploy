ea088fa09d53ff9fa42c9edaa4e20b53c8b20740
MJMarijnissen <kubamarijnissen@gmail.com>
2020-04-26 10:59:09 +0200

    L3H3 added cookies and logout

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import secrets
+import hashlib
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, Cookie, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+app.users = {"trudnY": "PaC13Nt"}
+app.sessions = {}
 
 security = HTTPBasic()
+KEY = "Silly key is silly, but not as silly as no key"
+
+def create_token(username: str, password: str):
+    token = hashlib.sha256(bytes(f"{username}{password}{KEY}", "utf-8"))
+    token = token.hexdigest()
+    return token
+
+
+def authenticate(username: str, password: str):
+    if username not in app.users or password != app.users[username]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user or password"
+        )
+    token = create_token(username, password)
+    app.sessions[token] = username
+    return token
 
 @app.get('/')
 def hello_world():
@@ -18,13 +37,13 @@ def hello_welcome():
 
 @app.post('/login')
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+    ses_token = authenticate(credentials.username, credentials.password)
     response = RedirectResponse(url='/welcome', status_code=status.HTTP_302_FOUND)
+    response.set_cookie(key="ses_token", value=ses_token)
     return response
+
+@app.post('/logout')
+def logout_user(ses_token: str = Cookie(None)):
+	del app.sessions[ses_token]
+	response = RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+	return response
